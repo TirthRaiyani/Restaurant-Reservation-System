d7767706e6a3b0589dfa1f7efefed2cd58582f61
TirthRaiyani <pateltirth220@gmail.com>
2024-08-12 16:43:24 +0530

    "Added console log statement to sendWelcomeEmail function in nodemailer.js"

--- backend/utils/nodemailer.js
+++ backend/utils/nodemailer.js
@@ -1,16 +1,17 @@
 const transporter = require('../config/mailer');
 const env = require('../config/env')
 
+
 const sendWelcomeEmail = async (email, username) => {
     const mailOptions = {
         from: env.EMAIL_USER,
         to: email,
         subject: 'Welcome Done',
         text: `Hello ${username},\n\nWelcome to Gotham`,
-
+        
     };
-
     await transporter.sendMail(mailOptions);
+    console.log('Email sent successfully');
 };
 
 // const sendResetEmail = async (email, token) => {
